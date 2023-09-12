6506a3d3e4057db468a59ad345ec35fdb21ac089
piyush singh bora <singhbora313@gmail.com>
2023-09-12 22:45:18 +0530

    deleted script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.oneclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classlist.toggle('open');
-};
